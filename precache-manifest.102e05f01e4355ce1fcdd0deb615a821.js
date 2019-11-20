5346188894e65f9023ef56f91bde95f0ad5b2d10
edumoreira1506 <contato@eduardoem.com.br>
2019-11-20 10:29:05 -0300

    Updates

---
+++ precache-manifest.102e05f01e4355ce1fcdd0deb615a821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8604070697f7522693c5e53e73af2ce9",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "1e58006f4f1820616cd0",
+    "url": "/blog/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3329ee98ec6033567e97",
+    "url": "/blog/static/js/2.acc59e0e.chunk.js"
+  },
+  {
+    "revision": "1e58006f4f1820616cd0",
+    "url": "/blog/static/js/main.ad44388f.chunk.js"
+  },
+  {
+    "revision": "64ed4be0dd873e71b762",
+    "url": "/blog/static/js/runtime-main.2bc6cafa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blog/static/media/logo.25bf045c.svg"
+  }
+]);
