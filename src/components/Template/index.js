201c9e776ea3d6050eecb7042ec705241a70d427
edumoreira1506 <contato@eduardoem.com.br>
2019-11-21 14:41:22 -0300

    Change folder strcuture

--- src/components/Template/index.js
+++ src/components/Template/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link, HashRouter } from 'react-router-dom'
 
+import './Template.scss'
+
 const Template = ({
   children
 }) =>
-  <div>
+  <div className="Template">
     <HashRouter basename="/">
       <Link to="/">Home</Link>
       <Link to="/about">Sobre</Link>
